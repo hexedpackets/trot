0fa69a211b468572f0956eea5515b4d1e90824f7
William Huba <williamhuba@gmail.com>
2015-05-12 23:22:20 -0400

    New mix project

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule Trot.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :trot,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: [:logger],
+     mod: {Trot, []}]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    []
+  end
+end
