4bc841f07e75c15f7c00fba910be4ceb50472328
William Huba <william@trywildcard.com>
2015-05-21 17:13:17 -0400

    When MIX_ENV=dev, eval templates from disk instead of compiling them into functions.

--- lib/template.ex
+++ lib/template.ex
@@ -24,12 +24,15 @@ defmodule Trot.Template do
   defmacro __using__(_opts) do
     quote do
       import Trot.Template
+
+      @template_root Path.relative_to_cwd("templates")
+
       @before_compile Trot.Template
     end
   end
 
   defmacro __before_compile__(env) do
-    template_root = Module.get_attribute(env.module, :template_root) || "templates"
+    template_root = Module.get_attribute(env.module, :template_root)
     template_files = Trot.Template.find_all(template_root)
     templates = Trot.Template.compile(template_files, template_root)
 
@@ -38,10 +41,17 @@ defmodule Trot.Template do
       Returns the template root alongside all template filenames.
       """
       def __templates__ do
-        {unquote(template_root), unquote(template_files)}
+        {@template_root, unquote(template_files)}
       end
 
       unquote(templates)
+
+      @doc """
+      Returns true whenever the list of templates changes in the filesystem.
+      """
+      def __template_recompile__? do
+        unquote(hash(template_root)) != Trot.Template.hash(@template_root)
+      end
     end
   end
 
@@ -50,11 +60,19 @@ defmodule Trot.Template do
   """
   def compile(files, root) when is_list(files) do
     files
-    |> Enum.map(&(compile(&1, root)))
+    |> Enum.map(&(compile(&1, root, pre_compile_templates)))
   end
-  def compile(file, root) when is_binary(file) do
-    quoted = EEx.compile_file(file)
+  def compile(file, root, _pre_compile = false) when is_binary(file) do
     file_match = Path.relative_to(file, root)
+    quote do
+      def render_template(unquote(file_match), assigns) do
+        EEx.eval_file(unquote(file), assigns: assigns)
+      end
+    end
+  end
+  def compile(file, root, _pre_compile = true) when is_binary(file) do
+    file_match = Path.relative_to(file, root)
+    quoted = EEx.compile_file(file)
     quote do
       def render_template(unquote(file_match), var!(assigns)) do
         unquote(quoted)
@@ -69,5 +87,19 @@ defmodule Trot.Template do
   def find_all(root) do
     Path.join(root, "**.eex")
     |> Path.wildcard
+  end
+
+  @doc """
+  Returns the hash of all template paths in the given root.
+  """
+  @spec hash(String.t) :: binary
+  def hash(root) do
+    find_all(root)
+    |> Enum.sort
+    |> :erlang.md5
+  end
+
+  defp pre_compile_templates do
+    Application.get_env(:mix, :env) != :dev
   end
 end
