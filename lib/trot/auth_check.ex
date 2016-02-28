062a4c950554fa24a2a1fbef5ccb150d6b1197d8
William Huba <hexedpackets@gmail.com>
2016-02-28 13:48:57 -0500

    New plug, Trot.AuthCheck, for requiring authorization on a select set of routes.

---
+++ lib/trot/auth_check.ex
@@ -0,0 +1,62 @@
+defmodule Trot.AuthCheck do
+  @moduledoc """
+  Checks that a user has an authorized session before allowing them to access certain routes.
+  By default, all routes are allowed.
+
+  ## Options
+  `routes`: List of URLs to check authorization on. Defaults to `[]`.
+  `match_fun`: Function to run when a request matches the routes. Defaults to returning a 403.
+  `match_default`: Function to run when a request does not match any configured routes. Defaults to passing the connection through.
+
+  ## Example:
+
+      defmodule PiedPiper do
+        use Plug.Builder
+        plug Trot.AuthCheck, [routes: ["/secrets"]]
+        use Trot.AuthCheck
+
+        get "/marketing", do: "We are amazing, srsly you guys."
+        get "/secrets", do: "We're actually about to go bankrupt"
+      end
+  """
+
+  import Plug.Conn
+  @behaviour Plug
+
+  @doc false
+  def init(opts) do
+    routes = Keyword.get(opts, :routes, [])
+    |> Enum.map(fn(route) -> Plug.Router.Utils.build_path_match(route) |> elem(1) end)
+
+    opts
+    |> Keyword.put(:routes, routes)
+    |> Keyword.put_new(:match_fun, &__MODULE__.private_route/1)
+    |> Keyword.put_new(:default_fun, &__MODULE__.public_route/1)
+  end
+
+  @doc false
+  def call(conn, opts) do
+    Enum.map(conn.path_info, &URI.decode/1)
+    |> check_route(opts[:routes], opts, conn)
+  end
+
+  defp check_route(_, [], opts, conn), do: apply(opts[:default_fun], [conn])
+  defp check_route(path, [path | _], opts, conn), do: apply(opts[:match_fun], [conn])
+  defp check_route(path, [_ | routes], opts, conn), do: check_route(path, routes, opts, conn)
+
+  @doc """
+  Called when a given route is publicly accessible. This will simply pass
+  the connection through to the next plug.
+  """
+  def public_route(conn), do: conn
+
+  @doc """
+  Called when a route needs to be authenticated. Returns a 403 if there is no authentication,
+  and passes the conn through untouched otherwise.
+  """
+  def private_route(conn) do
+    conn
+    |> send_resp(403, "")
+    |> halt
+  end
+end
---
+++ test/trot/auth_check_test.exs
@@ -0,0 +1,40 @@
+defmodule Trot.AuthCheckTest do
+  use ExUnit.Case, async: true
+  import Trot.TestHelper
+
+  defmodule AuthCheckRouter do
+    use Plug.Builder
+    plug Trot.AuthCheck, [routes: ["/secret"]]
+    use Trot.Router
+
+    get "/", do: :ok
+    get "/secret", do: "secret squirrel!"
+  end
+
+  test "public route is allowed through" do
+    conn = call(AuthCheckRouter, :get, "/")
+    assert conn.status == 200
+  end
+
+  test "default route returns unauthorized" do
+    conn = call(AuthCheckRouter, :get, "/secret")
+    assert conn.status == 403
+  end
+
+  defmodule CustomAuthCheckRouter do
+    use Plug.Builder
+    plug Trot.AuthCheck, [routes: ["/secret"], match_fun: &CustomAuthCheckRouter.secret_resp/1]
+    use Trot.Router
+
+    get "/secret", do: "secret squirrel!"
+
+    def secret_resp(conn) do
+      conn |> send_resp(420, "") |> halt
+    end
+  end
+
+  test "custom auth match function" do
+    conn = call(CustomAuthCheckRouter, :get, "/secret")
+    assert conn.status == 420
+  end
+end
